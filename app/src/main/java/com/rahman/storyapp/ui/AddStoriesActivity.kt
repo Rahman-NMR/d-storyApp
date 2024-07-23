4f2648372798f7ef8409c638f8f41e4333f2fc6b
Rahman-NMR <49480009+Rahman-NMR@users.noreply.github.com>
2024-07-23 11:32:43 +0700

    add AddStoriesActivity

---
+++ app/src/main/java/com/rahman/storyapp/ui/AddStoriesActivity.kt
@@ -0,0 +1,24 @@
+package com.rahman.storyapp.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+import com.rahman.storyapp.R
+
+class AddStoriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_stories)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_add_stories)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<MaterialToolbar>(R.id.top_app_bar_add_stories).setNavigationOnClickListener { finish() }
+    }
+}
