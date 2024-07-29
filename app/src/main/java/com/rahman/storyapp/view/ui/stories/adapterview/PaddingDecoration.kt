64df01122959bc065c4795151f2436c3e222e8bf
Rahman-NMR <49480009+Rahman-NMR@users.noreply.github.com>
2024-07-29 20:54:40 +0700

    adjust padding recyclerview

--- app/src/main/java/com/rahman/storyapp/view/ui/stories/adapterview/PaddingDecoration.kt
+++ app/src/main/java/com/rahman/storyapp/view/ui/stories/adapterview/PaddingDecoration.kt
@@ -5,16 +5,26 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class PaddingDecoration(private val context: Context, private val padding: Int) : RecyclerView.ItemDecoration() {
-    private fun dpToPx(): Int {
-        return (padding * context.resources.displayMetrics.density).toInt()
+class PaddingDecoration(context: Context, private val padding: Int) : RecyclerView.ItemDecoration() {
+    private val displayMetrics = context.resources.displayMetrics
+
+    private fun dpToPx(dp: Int): Int {
+        return (dp * displayMetrics.density).toInt()
     }
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         super.getItemOffsets(outRect, view, parent, state)
+        val screenWidthPx = displayMetrics.widthPixels
+
+        val horizontalPadding = if (screenWidthPx > dpToPx(500)) {
+            (screenWidthPx - dpToPx(500)) / 2
+        } else 0
+
+        outRect.left = horizontalPadding
+        outRect.right = horizontalPadding
 
         if (parent.getChildAdapterPosition(view) < state.itemCount - 1) {
-            outRect.bottom = dpToPx()
+            outRect.bottom = dpToPx(padding)
         }
     }
 }
