6a3faa72973121a2df2a4d04d7cd16130c7249ef
Rahman-NMR <49480009+Rahman-NMR@users.noreply.github.com>
2024-08-01 13:59:28 +0700

    app version 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rahman.storyapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BASE_URL", "\"${findProperty("base_url")}\"")
